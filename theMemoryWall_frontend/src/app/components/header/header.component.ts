bf06c395cf96355909acdd2f622de0cf34701387
Weqil <ar5ipooshka@yandex.ru>
2024-07-15 16:27:00 +0500

    add back button

--- theMemoryWall_frontend/src/app/components/header/header.component.ts
+++ theMemoryWall_frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BackButtonComponent } from '../back-button/back-button.component';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    BackButtonComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
