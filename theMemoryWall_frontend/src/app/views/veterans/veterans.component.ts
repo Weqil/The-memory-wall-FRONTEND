28201a95e92b625e650d160e9445b5daf89d2b0e
Weqil <ar5ipooshka@yandex.ru>
2024-07-17 16:12:45 +0500

    add new card

--- theMemoryWall_frontend/src/app/views/veterans/veterans.component.ts
+++ theMemoryWall_frontend/src/app/views/veterans/veterans.component.ts
@@ -73,9 +73,9 @@ export class VeteransComponent  implements OnInit {
         layout: {
           default: [
             'ё й ц у к е н г ш щ з х ъ {bksp}',
-            'ф ы в а п р о л д ж э',
-            'я ч с м и т ь б ю',
-            '{space}'
+            'ф ы в а п р о л д ж э я',
+            'ч с м и т ь б ю {space}',
+           
           ],
         },
         display: {
