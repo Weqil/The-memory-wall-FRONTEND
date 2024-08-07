a0c16e7e08dc41a8c4b26e2c2e00440440945ee7
Arrsenii <114972938+Weqil@users.noreply.github.com>
2024-08-07 12:03:37 +0500

    add contacts

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(
+    private router: Router
+  ){}
+  url:string = ''
 
+  ngOnInit(): void {
+    this.router.events.subscribe((events:any)=>{
+      this.url = events.url
+    })
+  }
 }
